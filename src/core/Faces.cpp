55ff32562912f5509f522cd0b38277d7c353b9be
Tobias Moraut <tobiasmoraut7@gmail.com>
2025-09-08 20:25:00 +0000

    Faces.cpp advancement

--- src/core/Faces.cpp
+++ src/core/Faces.cpp
@@ -38,46 +38,101 @@
 #include "Faces.hpp"
   
 Faces::Faces(const int nV, const vector<int>& coordIndex) {
-  // TODO
+  this->_indices = coordIndex;
+  int maxIndex = *max_element(_indices.begin(), _indices.end());
+
+  if(nV < maxIndex + 1){
+    this-> _numVertices = maxIndex + 1;
+  
+  } else {
+      this->_numVertices = nV;
+    }
+
 }
 
 int Faces::getNumberOfVertices() const {
-  // TODO
-  return 0;
-}
+  return _numVertices;
+}  
 
 int Faces::getNumberOfFaces() const {
-  // TODO
-  return 0;
+  int numberOfFaces = 0;
+  for (int i = 0; i < _indices.size(); i++){
+    if (_indices[i] == -1){
+      numberOfFaces++;
+    }
+  }
+  return numberOfFaces;
 }
 
 int Faces::getNumberOfCorners() const {
-  // TODO
-  return 0;
+  return this->_indices.size();
 }
 
 int Faces::getFaceSize(const int iF) const {
-  // TODO
-  return 0;
+  int numFaces = getNumberOfFaces();
+    if(iF < 0 || iF >= numFaces) 
+        return -1;
+
+    int faceCounter = 0;
+    int index = 0;
+    while(index < _indices.size() && faceCounter < iF){
+      if(_indices[index] == -1)
+        faceCounter++;
+      index++;
+    }
+
+    int faceSize = 0;
+    while(index < _indices.size() && _indices[index] != -1){
+      faceSize++;
+      index++;
+    }
+    return faceSize;
 }
 
 int Faces::getFaceFirstCorner(const int iF) const {
-  // TODO
-  return -1;
+    int numFaces = getNumberOfFaces();
+    if(iF < 0 || iF >= numFaces) 
+        return -1;
+
+    int faceCounter = 0;
+    for(int i = 0; i < _indices.size(); ++i) {
+        if(faceCounter == iF) {
+            return i; 
+        }
+        if(_indices[i] == -1) {
+            faceCounter++;
+        }
+    }
+
+    return -1; 
 }
 
 int Faces::getFaceVertex(const int iF, const int j) const {
-  // TODO
-  return -1;
+  int numFaces = getNumberOfFaces();
+  if(iF < 0 || iF >= numFaces) 
+        return -1;
+
+  int firstCornerIndex = getFaceFirstCorner(iF);
+  int faceSize = getFaceSize(iF);
+
+  if(j <= faceSize && j >= 0){
+    return _indices[firstCornerIndex + j - 1];
+  }
+  else return -1;
 }
 
 int Faces::getCornerFace(const int iC) const {
-  // TODO
-  return -1;
+  if(iC < 0 || iC >= _indices.size())
+    return -1;
+
+  int faceCount = 0;
+  for(int i = 0; i < iC; i++){
+    if(_indices[i] == -1)
+      faceCount++;
+  }
+  return faceCount;
 }
 
 int Faces::getNextCorner(const int iC) const {
-  // TODO
   return -1;
 }
-
